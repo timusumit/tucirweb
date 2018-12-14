37687f75723d34b2f8cd8c25e73e225379bff0c4
Sumit <timusumit@gmail.com>
2018-12-14 16:30:07 +0545

    refining a bit

--- application/controllers/admin/Create_post.php
+++ application/controllers/admin/Create_post.php
@@ -6,8 +6,8 @@ class Create_post extends Admin_controller{
 		parent::__construct();
 		  $this->load->model('create_post_model');
 		  $this->load->model('user_model');
-		    $this->load->helper(array('form', 'url'));
-        $this->load->library(array('session', 'form_validation'));
+		  $this->load->helper(array('form', 'url'));
+      $this->load->library(array('session', 'form_validation'));
 	}
 
 
@@ -21,9 +21,6 @@ public function index(){
 /* for adding post */
 public function do_upload()
         {
-
-
-
                 $config['upload_path']          = './site_assets/uploads/blog';
                 $config['allowed_types']        = 'jpg|jpeg|png|gif|svg';
                 $config['file_name']='blog_image';
@@ -31,64 +28,47 @@ public function do_upload()
 
                 $this->load->library('upload', $config);
                 $file=$this->upload->do_upload('userfile');
-                 $uploadedImage = $this->upload->data();
-   //    echo $uploadedImage['file_name'];exit;
-        $this->resizeImage($uploadedImage['file_name']);
-
+                $uploadedImage = $this->upload->data();
+                $this->resizeImage($uploadedImage['file_name']);
+               /* if file is not chosen*/
                 if ( ! $file )
                 {
-                 // echo "no file chosen";exit;
-$test = $this->uri->segment(4);
+                    $test = $this->uri->segment(4);
+                    if($test==0)
+                        {
+                          $post_id=0;
+                        }
+                    else{
+                          $post_id = $this->uri->segment(4);
+                        }
 
-if($test==0)
-                    {
-                        $post_id=0;
-                     
-                    }
-                else {
-                  
-                   $post_id = $this->uri->segment(4);
-  
+                      $create_post = $this->create_post_model->get_post_image_byid($post_id);
+                      $filename=$create_post['post_image_name'];
+                      $filepath=$create_post['post_image_url'];             
+                            $this->create_post_model->set_post_image_name($post_id,$filename,$filepath);
+                      redirect('admin/create_post',$data);
                 }
-
-                      /*  $data = array(
-                            'file_name' => 'no_image.jpg',
-                            'full_path' => 'no_path',
-                    );        */ 
-
-                      $post_id=$this->uri->segment(4);
-            $create_post = $this->create_post_model->get_post_image_byid($post_id);
-         $filename=$create_post['post_image_name'];
-         $filepath=$create_post['post_image_url'];             
-                        $this->create_post_model->set_post_image_name($post_id,$filename,$filepath);
-                        redirect('admin/create_post',$data);
-
-                }
+                /* if file is chosen */
                 else
                 {
-
-$test = $this->uri->segment(4);
-
-if($test==0)
-                    {
-                        $post_id=0;
-                     
-                    }
-                else {
-                  
-                   $post_id = $this->uri->segment(4);
-  
-                }
-                   $create_post = $this->create_post_model->get_post_image_byid($post_id);
-                  $old_file_path=$create_post['post_image_url'];
-                  $old_tn_path=$_SERVER['DOCUMENT_ROOT'] . '/tucirweb/site_assets/uploads/blog/thumbnail/'.$create_post['post_image_name'];
-
+                  $test = $this->uri->segment(4);
+                  if($test==0)
+                      {
+                          $post_id=0;
+                      }
+                  else{
+                        $post_id = $this->uri->segment(4);
+                      }
+                        $create_post = $this->create_post_model->get_post_image_byid($post_id);
+                        $old_file_path=$create_post['post_image_url'];
+                        $old_tn_path=$_SERVER['DOCUMENT_ROOT'] . '/tucirweb/site_assets/uploads/blog/thumbnail/'.$create_post['post_image_name'];
+                        
                         $data = array(
                             'file_name' => $this->upload->data('file_name'),
                             'full_path' => $this->upload->data('full_path'),
-                    );                      
+                                      );                      
                        	$this->create_post_model->set_post_image_name($post_id,$data['file_name'],$data['full_path']);
-
+                        
                         unlink($old_file_path);
                         unlink($old_tn_path);
                         redirect('admin/create_post',$data);
@@ -100,29 +80,7 @@ if($test==0)
 
 
       public function edit(){
-
-         /* $this->load->library('upload');
-          $file=$this->upload->do_upload('userfile');
-            if ( ! $file )
-                {
-                  echo "test";exit;
-                  $post_id=$this->uri->segment(4);
-            $create_post = $this->create_post_model->get_post_image_byid($post_id);
-         $filename=$create_post['post_image_name'];
-         $filepath=$create_post['post_image_url'];
-                   $post_id = $this->uri->segment(4);
-                 $this->create_post_model->set_post_image_name($post_id,$filename,$full_path);
-                }else{
-                  echo "tesdt";exit;
-                }*/
-
-$post_id=$this->uri->segment(4);
-//echo $post_id;exit;
-        $create_post = $this->create_post_model->get_post_image_byid($post_id);
-        $old_file_path=$create_post['post_image_url'];
-        $old_tn_path=$_SERVER['DOCUMENT_ROOT'] . '/tucirweb/site_assets/uploads/blog/thumbnail/'.$create_post['post_image_name'];
         $this->do_upload();
-       // echo $old_file_path;exit;
          redirect('admin/create_post');       
          }
 
